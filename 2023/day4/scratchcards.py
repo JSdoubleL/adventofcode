51e3f88c89ac2bb763916a7e3a46981854d1be67
JSdoubleL <jsdoublel@gmail.com>
2023-12-04 00:32:28 -0600

    day4 part 2

--- 2023/day4/scratchcards.py
+++ 2023/day4/scratchcards.py
@@ -1,18 +1,24 @@
 import argparse
 from time import time
 
-def score_card(game: str) -> int:
+def score_card(card: str) -> int:
     winning, yours = [side.split()
-                      for side in game.strip().split(': ')[1].split(' | ')]
-    return int(2 ** (sum(1 for n in yours if n in winning) - 1))
+                      for side in card.strip().split(': ')[1].split(' | ')]
+    return sum(1 for n in yours if n in winning)
 
 def part_1(filepath: str) -> int:
     with open(filepath, "r") as f:
-        return sum(score_card(line) for line in f)
+        return sum(int(2 ** (score_card(line) - 1)) for line in f)
 
 def part_2(filepath: str) -> int:
     with open(filepath, "r") as f:
-        pass
+        data = [line for line in f]
+    card_counter = [1] * len(data)
+    for i, card in enumerate(data):
+        score = score_card(card)
+        for j in range(1, score + 1):
+            card_counter[i + j] += card_counter[i]
+    return sum(card_counter)
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
@@ -23,6 +29,6 @@ if __name__=="__main__":
     t = time()
     print(f"Part 1: {part_1(input_file):<10d}", 
           f"|{((time() - t) * 1000):10.2f} milliseconds", sep="")
-    # t = time()
-    # print(f"Part 2: {part_2(input_file):<10d}", 
-    #       f"|{((time() - t) * 1000):10.2f} milliseconds", sep="")
+    t = time()
+    print(f"Part 2: {part_2(input_file):<10d}", 
+          f"|{((time() - t) * 1000):10.2f} milliseconds", sep="")
